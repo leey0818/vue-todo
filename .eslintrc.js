a20f4ab25354df4a6bbfb9b402ab065064d2e6e7
leey0818 <leey0818@gmail.com>
2019-09-23 00:20:51 +0900

    lint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-plusplus': ['error', {
+      allowForLoopAfterthoughts: true,
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
