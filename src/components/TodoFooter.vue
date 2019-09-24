d5345168dbb611438f740dc9deb90890d9aac5f2
leey0818 <leey0818@gmail.com>
2019-09-24 21:38:25 +0900

    clear all 기능 구현

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    Footer
+    <v-btn color="red darken-3" outlined @click="clearAll">
+      <i class="fa fa-trash-alt mr-1"></i> Clear All
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearAll() {
+      window.sessionStorage.clear();
+    },
+  },
 };
 </script>
 
