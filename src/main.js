c76934f5636032b592ecff11ae75366d7ec8d0f8
leey0818 <leey0818@gmail.com>
2019-09-29 16:45:20 +0900

    vuex 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 import vuetify from './plugins/vuetify';
 import './assets/lib/fontawesome/css/all.min.css';
 
@@ -7,5 +8,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App),
 }).$mount('#app');
